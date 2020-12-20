2dd9f3cbc13a589acb86919c837835d9a46baf8e
William Prieto <wprietovelandia@gmail.com>
2020-12-20 17:21:16 -0500

    Test de usuario agregado

--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -5,10 +5,10 @@ const auth = require('../../middlewares/auth.js');
 const router = routerx();
 
 router.post('/login', usuarioController.login);
-router.get('/list', auth.verifyAdministrador, usuarioController.list); //pagina.com/api/usuario/list
-router.post('/add', auth.verifyAdministrador, usuarioController.add); //pagina.com/api/usuario/add
-router.put('/update', auth.verifyAdministrador, usuarioController.update); //pagina.com/api/usuario/update
-router.put('/activate', auth.verifyAdministrador, usuarioController.activate); //pagina.com/api/usuario/activate
-router.put('/deactivate', auth.verifyAdministrador, usuarioController.deactivate);  //pagina.com/api/usuario/deactivate
+router.get('/list', usuarioController.list); //pagina.com/api/usuario/list
+router.post('/add', usuarioController.add); //pagina.com/api/usuario/add
+router.put('/update', usuarioController.update); //pagina.com/api/usuario/update
+router.put('/activate', usuarioController.activate); //pagina.com/api/usuario/activate
+router.put('/deactivate', usuarioController.deactivate);  //pagina.com/api/usuario/deactivate
 
 module.exports = router;
